0c045b0bad4dc21a4243ce65d141aef982673b59
Tong Liu <qq276356648@126.com>
2021-02-24 22:00:53 +0800

    Added Typescript code for test taker WebRTC and corresponding .NET helper. Previous versions of wrappers and JS files are removed

---
+++ Client/WebRTCInterop/WebRTCClientTaker.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+using BrowserInterop.Extensions;
+using Microsoft.JSInterop;
+
+namespace SmartProctor.Client.WebRTCInterop
+{
+    public class WebRTCClientTaker
+    {
+        private IJSRuntime _jsRuntime;
+        private JsRuntimeObjectRef _jsObj;
+        
+        public event EventHandler<string> OnIceCandidate;
+        public event EventHandler<string> OnLocalSdp;
+        public event EventHandler<string> OnIceConnectionStateChange;
+        public event EventHandler<string> OnConnectionStateChange;
+
+        private WebRTCClientTaker(IJSRuntime jsRuntime, JsRuntimeObjectRef jsObj)
+        {
+            _jsObj = jsObj;
+            _jsRuntime = jsRuntime;
+        }
+
+        public async ValueTask<WebRTCClientTaker> GetInstance(IJSRuntime jsRuntime)
+        {
+            var window = await jsRuntime.GetWindowPropertyRef("SmartProctor")
+                .ConfigureAwait(false);
+            var obj = await jsRuntime.InvokeInstanceMethodGetRef(window, "getWebRTCClientTaker")
+                .ConfigureAwait(false);
+            
+            var ret = new WebRTCClientTaker(jsRuntime, obj);
+            await ret.ConfigureCallbacks();
+            
+            return ret;
+        }
+
+        public async ValueTask StartStreaming()
+        {
+            await _jsRuntime.InvokeInstanceMethod(_jsObj, "startStreaming").ConfigureAwait(false);
+        }
+
+        public async ValueTask ReceivedAnswerSDP(string sdp)
+        {
+            await _jsRuntime.InvokeInstanceMethod(_jsObj, "receivedAnswerSDP", sdp).ConfigureAwait(false);
+        }
+
+        public async ValueTask ReceivedIceCandidate(string candidate)
+        {
+            await _jsRuntime.InvokeInstanceMethod(_jsObj, "receivedIceCandidate", candidate).ConfigureAwait(false);
+        }
+
+        public async ValueTask<string> GetIceConnectionState()
+        {
+            return await _jsRuntime.InvokeInstanceMethod<string>(_jsObj, "getIceConnectionState").ConfigureAwait(false);
+        }
+
+        public async ValueTask<string> GetConnectionState()
+        {
+            return await _jsRuntime.InvokeInstanceMethod<string>(_jsObj, "getConnectionState").ConfigureAwait(false);
+        }
+
+        private async ValueTask ConfigureCallbacks()
+        {
+            await _jsRuntime.AddEventListener(_jsObj, "self", "onIceCandidate", CallBackInteropWrapper.Create(
+                async (string candidate) =>
+                {
+                    OnIceCandidate?.Invoke(this, candidate);
+                })).ConfigureAwait(false);
+            await _jsRuntime.AddEventListener(_jsObj, "self", "onLocalSdp", CallBackInteropWrapper.Create(
+                async (string candidate) =>
+                {
+                    OnLocalSdp?.Invoke(this, candidate);
+                })).ConfigureAwait(false);
+            await _jsRuntime.AddEventListener(_jsObj, "self", "onIceConnectionStateChange", CallBackInteropWrapper.Create(
+                async (string candidate) =>
+                {
+                    OnIceConnectionStateChange?.Invoke(this, candidate);
+                })).ConfigureAwait(false);
+            await _jsRuntime.AddEventListener(_jsObj, "self", "onConnectionStateChange", CallBackInteropWrapper.Create(
+                async (string candidate) =>
+                {
+                    OnConnectionStateChange?.Invoke(this, candidate);
+                })).ConfigureAwait(false);
+        }
+    }
+}
---
+++ Client/wwwroot/js/WebRTCClientTaker.ts
@@ -0,0 +1,90 @@
+﻿declare global {
+    // The Typescript translator won't recognize those
+    // if I do not declare this
+    interface Window {
+        SmartProctor: any;
+    }
+    
+    interface Element {
+        srcObject: any;
+    }
+}
+
+window.SmartProctor = window.SmartProctor || {};
+
+namespace SmartProctor {
+    /**
+     * Typescript implementation of the WebRTC related functions in the test-taker side
+     */
+    export class WebRTCClientTaker extends EventTarget {
+        private connection: RTCPeerConnection;
+        private stream: MediaStream;
+        
+        // Callbacks that will be used in .NET
+        public onIceCandidate: {(this: WebRTCClientTaker, candidate: string): void };
+        public onLocalSdp: {(this: WebRTCClientTaker, sdp: string): void };
+        public onIceConnectionStateChange: {(this: WebRTCClientTaker, state: string): void };
+        public onConnectionStateChange: {(this: WebRTCClientTaker, state: string): void };
+        
+        public init() {
+            this.connection = new RTCPeerConnection();
+            this.connection.addTransceiver("video", {direction: 'sendonly'})
+            this.connection.onicecandidate = (e) => {
+                // Send the ICE candidate info through SignalR in .NET
+                this.onIceCandidate(JSON.stringify(e.candidate));
+            }
+            
+            this.connection.oniceconnectionstatechange = (e) => {
+                this.onIceConnectionStateChange(this.connection.iceConnectionState);
+            }
+            
+            this.connection.onconnectionstatechange = (e) => {
+                this.onConnectionStateChange(this.connection.connectionState);
+            }
+        }
+        
+        public async startStreaming() {
+            this.stream = await navigator.mediaDevices.getUserMedia({ video : true });
+            var localVideo = document.querySelector(".video.local");
+            localVideo.srcObject = this.stream;
+            
+            this.stream.getTracks().forEach((track) => {
+                this.connection.addTrack(track, this.stream);
+            });
+            
+            var offer = await this.connection.createOffer();
+            await this.connection.setLocalDescription(offer);
+            
+            // Send the local SDP through SignalR in .NET
+            this.onLocalSdp(JSON.stringify(offer));
+        }
+        
+        public async receivedAnswerSDP(sdp) {
+            await this.connection.setRemoteDescription(JSON.parse(sdp));
+        }
+        
+        public async receivedIceCandidate(candidate) {
+            await this.connection.addIceCandidate(JSON.parse(candidate));
+        }
+        
+        public getIceConnectionState() {
+            return this.connection.iceConnectionState;
+        }
+        
+        public getConnectionState() {
+            return this.connection.connectionState;
+        }
+    }
+    
+}
+
+var webRTCClientTaker: SmartProctor.WebRTCClientTaker;
+
+window.SmartProctor.getWebRTCClientTaker = () => {
+    if (webRTCClientTaker == null) {
+        webRTCClientTaker = new SmartProctor.WebRTCClientTaker();
+        webRTCClientTaker.init();
+    }
+    
+    return webRTCClientTaker;
+}
